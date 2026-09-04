20c329591a7783019ee15f64148114200b56a1c3
soenneker <jake@soenneker.com>
2026-09-03 22:01:51 -0500

    Add interface inheritdoc references

--- src/Soenneker.Validators.BasicAuth/BasicAuthValidator.cs
+++ src/Soenneker.Validators.BasicAuth/BasicAuthValidator.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Http;
 
 namespace Soenneker.Validators.BasicAuth;
 
+/// <inheritdoc cref="IBasicAuthValidator" />
 public sealed class BasicAuthValidator : Validator.Validator, IBasicAuthValidator
 {
     private readonly IConfiguration _configuration;
